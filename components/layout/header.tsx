2156f1a3b984dd6f4f99d3e6e85584d081433c26
lanhkute <lananhkulzsc@gmail.com>
2025-03-17 23:25:37 +0700

    anh

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -42,10 +42,21 @@ export default function Header() {
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between h-20">
           {/* Logo */}
-          <Link href="/" className="flex items-center space-x-2">
+          {/* <Link href="/" className="flex items-center space-x-2">
             <div className="w-10 h-10 bg-red-700 rounded-full flex items-center justify-center">
               <span className="font-bold text-lg">VM</span>
             </div>
+            <span className="font-bold text-xl hidden md:inline-block">
+              Bảo tàng LSQS Việt Nam
+            </span>
+          </Link> */}
+
+          <Link href="/" className="flex items-center space-x-2">
+            <img
+              src=""
+              alt="Logo Bảo tàng LSQS Việt Nam"
+              className="w-12 h-12 object-contain"
+            />
             <span className="font-bold text-xl hidden md:inline-block">
               Bảo tàng LSQS Việt Nam
             </span>
