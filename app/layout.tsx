357b75bb31bcdfd760d792003f2f3497d6ca0eb8
DESKTOP-HU69O7Q\tuanchill <tuanlt203@gmail.com>
2025-04-03 01:55:49 +0700

    feat: add favicon support in layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,10 @@ export const metadata: Metadata = {
   title: "Bảo tàng LSQS Việt Nam",
   description:
     "Bảo tàng Lịch sử Quân sự Việt Nam [1][2] là một trong các bảo tàng quốc gia và đứng đầu trong hệ thống Bảo tàng Quân đội, hiện đang lưu giữ, trưng bày hơn 15 vạn tài liệu, hiện vật, trong đó có nhiều sưu tập độc đáo và 4 Bảo vật Quốc gia, gồm máy bay MiG-21 số hiệu 4324, máy bay MiG-21 số hiệu 5121, Bản đồ Quyết tâm chiến dịch Hồ Chí Minh và xe tăng T-54B số hiệu 843. Bảo tàng mới được mở cửa từ tháng 11 năm 2024, có địa chỉ tại Km 6+500 Đại Lộ Thăng Long, phường Tây Mỗ, Đại Mỗ, quận Nam Từ Liêm, thành phố Hà Nội. Trước đó, bảo tàng cũ tọa lạc tại số 28A đường Điện Biên Phủ, phường Điện Biên, quận Ba Đình, đối diện Công viên Lê-nin, nằm trong khuôn viên Hoàng thành Thăng Long.",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
